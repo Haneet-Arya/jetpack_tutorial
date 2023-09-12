3787074e9b556333e4945ff693e74194fad3b0d7
Haneet Arya <haneetarya16@gmail.com>
2023-09-13 00:11:34 +0530

    MusicKnob

--- app/src/main/java/com/haneetarya/ytphllipe/MusicKnob.kt
+++ app/src/main/java/com/haneetarya/ytphllipe/MusicKnob.kt
@@ -11,11 +11,14 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -36,6 +39,8 @@ import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawStyle
+import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.layout.boundsInWindow
@@ -53,6 +58,7 @@ class MusicKnob : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            /*
             Box(
                 contentAlignment = Alignment.Center,
                 modifier = Modifier
@@ -69,7 +75,7 @@ class MusicKnob : ComponentActivity() {
                     var volume by remember {
                         mutableStateOf(0f)
                     }
-                    val barCount = 10
+                    val barCount = 20
                     MusicKnobComp(modifier = Modifier.size(100.dp)) {
                         volume = it
                     }
@@ -82,6 +88,66 @@ class MusicKnob : ComponentActivity() {
                         barCount = barCount
                     )
                 }
+            }
+            */
+
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0xFF101010))
+            ) {
+                Column(
+                    verticalArrangement = Arrangement.SpaceEvenly,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .border(1.dp, color = Color.Green, RoundedCornerShape(0.dp))
+                        .padding(10.dp)
+                        .fillMaxSize()
+                ) {
+                    var volume by remember {
+                        mutableStateOf(0f)
+                    }
+                    val barCount = 30
+                    VolumeBarVertical(
+                        modifier = Modifier
+                            .fillMaxHeight(0.8f)
+                            .width(10.dp),
+                        activeBar = (barCount * volume).roundToInt(),
+                        barCount = barCount
+                    )
+                    Spacer(modifier = Modifier.height(1.dp))
+                    MusicKnobComp(modifier = Modifier.size(100.dp)) {
+                        volume = it
+                    }
+                }
+            }
+
+        }
+    }
+}
+
+@Composable
+fun VolumeBarVertical(
+    modifier: Modifier = Modifier,
+    activeBar: Int = 0,
+    barCount: Int = 10
+) {
+    BoxWithConstraints(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.rotate(180f).offset(x = (-21).dp)
+    ) {
+        val barWidth = remember {
+            1500 / (2f * barCount)
+        }
+        Canvas(modifier = modifier) {
+            for (i in 1..barCount) {
+                drawRoundRect(
+                    color = if (i in 0..activeBar) Color.Green else Color.DarkGray,
+                    topLeft = Offset(0f, barWidth * 2f * i + barWidth),
+                    size = Size(150f, barWidth),
+                    cornerRadius = CornerRadius(0f)
+                )
             }
         }
     }
@@ -147,7 +213,7 @@ fun MusicKnobComp(
                 centerY = windowBounds.size.height / 2f
             }
             .pointerInteropFilter { event ->
-                // finding position of toch
+                // finding position of touch
                 touchX = event.x
                 touchY = event.y
                 // atan gives tan-1(x,y), minus is there because we are calculating angle oppposite to the normal angle calculation
@@ -170,11 +236,9 @@ fun MusicKnobComp(
                             true
                         } else false
                     }
-
                     else -> false
                 }
             }
             .rotate(rotation)
     )
-
 }
